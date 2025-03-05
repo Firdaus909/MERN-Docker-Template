8289938a496db80612a0e599bc86b8b75a7562da
Firdaus909 <muhammadzamzami109@gmail.com>
2025-03-05 22:02:51 +0700

    Make default db name

--- mongo-init.js
+++ mongo-init.js
@@ -1,3 +1,5 @@
-db = db.getSiblingDB('mern-db');
+const dbName = process.env.MONGO_DB_NAME || 'db';
+
+db = db.getSiblingDB(dbName);
 db.createCollection('users');
 db.users.insert({ name: 'John Doe', email: 'john@example.com' });
